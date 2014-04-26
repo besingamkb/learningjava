4a3142709d6aad39c3a08fee4917bd717ceac064
Mark Kevin Besinga <besingamkb@gmail.com>
2014-04-26 13:53:40 +0800

    create basic calculator with GUI

---
+++ calculator/calc.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class calc extends JFrame implements ActionListener {
+	public calc() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Calculator");
+		setResizable(false);
+		setSize(230, 370);
+		setLocationRelativeTo(null);
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+
+	}
+
+
+}
---
+++ calculator/run.java
@@ -0,0 +1,7 @@
+public class run {
+	public static calc c = new calc();
+
+	public static void main(String[] args) {
+		c.setVisible(true);
+	}
+}
